977b915d9eb6443ed0370e626f351f1c713c2a10
Rohit Tiwari <tigertiwari1023@gmail.com>
2025-01-31 13:41:59 +0530

    add: dashboard ui setup using shadcn

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,17 +1,12 @@
-import Header from "@/components/shared/header";
-import React from "react";
+import { AppSidebar } from "@/components/app-ui/sidebar-layout/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar-layout";
 import Image from "next/image";
 import backgroundLeft from "@/assets/backgrounds/docs-left.png";
 import backgroundRight from "@/assets/backgrounds/docs-right.png";
-import { Footer } from "@/components/shared/footer";
 
-function MainLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<main className="bg-background relative z-0 h-full w-full items-start justify-start">
+		<main className="bg-secondary-950 relative z-0 h-full w-full">
 			<div className="container-main left-[ min(calc(600px+15vw),calc(600px-2vw))] fixed top-0 -z-10 mx-auto w-full animate-pulse opacity-45 blur-[100px] !duration-[4s] dark:opacity-70">
 				<div className="bg-site-main-clip h-[600px] w-[600px] bg-pink-700/40 bg-blend-luminosity" />
 			</div>
@@ -27,13 +22,12 @@ function MainLayout({
 				className="rounded-large fixed -right-50 -bottom-20 -z-10 h-auto w-[80vw] animate-pulse opacity-0 shadow-none shadow-black/5 transition-transform !duration-[4s] data-[loaded=true]:opacity-50 motion-reduce:transition-none dark:data-[loaded=true]:opacity-100"
 				alt="docs left background"
 			/>
-			<div className="bg-background/10 relative z-10 flex h-full w-full flex-col overflow-y-auto bg-blend-screen backdrop-blur-md">
-				<Header />
-				{children}
-				<Footer />
+			<div className="bg-background/40 relative z-10 flex h-full w-full flex-col overflow-y-auto bg-blend-screen backdrop-blur-md">
+				<SidebarProvider>
+					<AppSidebar />
+					<SidebarInset>{children}</SidebarInset>
+				</SidebarProvider>
 			</div>
 		</main>
 	);
 }
-
-export default MainLayout;
